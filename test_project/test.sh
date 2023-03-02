34ab2368b7f0bcf6d5c4d6518fb7a6d01f8e55bb
Nikolai Perlt <nikolaiperlt@outlook.com>
2023-03-02 07:28:46 +0100

    pipeline test

--- test_project/test.sh
+++ test_project/test.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 # Declare an empty array to store the results
-results=()
+# results=()
+resultsStr=""
 for file in diagrams/*; do
     result=$(curl -s -X POST -F "image=@$file" https://imgurproxy-app-service.azurewebsites.net/upload)
-    results+=("$result")
-    echo "test"
+    # results+=("$result")
+    resultsStr+="$result"
 done
-echo ${results[@]}
-res=[$(echo ${results[@]} | sed 's/ /, /g')]
-echo $res
+echo $resultsStr
+echo "MARKDOWN=$resultsStr" >> $GITHUB_OUTPUT
+
+
+
+# echo ${results[@]}
+# res=[$(echo ${results[@]} | sed 's/ /, /g')]
+# echo $res
 # echo "MARKDOWN=$res" >> $GITHUB_OUTPUT
 # echo $GITHUB_OUTPUT
